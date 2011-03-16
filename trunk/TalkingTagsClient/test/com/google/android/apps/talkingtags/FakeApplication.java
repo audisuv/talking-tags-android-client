9570a9a65e80e195bc079465a1e95657c39b0add
adamconnors@gmail.com <adamconnors@gmail.com@2f18c655-9d46-4c27-46c3-8d257dfb4d8a>
2011-03-16 14:57:01 +0000

    Fix tests.

--- trunk/TalkingTagsClient/test/com/google/android/apps/talkingtags/FakeApplication.java
+++ trunk/TalkingTagsClient/test/com/google/android/apps/talkingtags/FakeApplication.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.google.android.apps.talkingtags;
 
@@ -22,6 +22,8 @@ import com.google.android.apps.talkingtags.threading.BlockingThreadQueue;
 import com.google.android.apps.talkingtags.threading.ThreadEndpoint;
 import com.google.android.apps.talkingtags.threading.ThreadQueue;
 
+import org.easymock.EasyMock;
+
 /**
  * Creates all the necessary parts for testing.
  * @author adamconnors
@@ -36,31 +38,33 @@ public class FakeApplication {
   public final TextDataSource tagSource;
   public final Controller ctrl;
   private FakeDataSource server;
-  
+
   public final AdminView fakeView;
-  
+  public final PlatformServices fakeServices;
+
   public FakeApplication() {
     tagSource = new UrlTextDataSource();
     tagParser = new OxTagDataParser();
     collectionParser = new CollectionParser();
     server = new FakeDataSource();
     network = new BlockingThreadQueue<Request>(new NetworkFetcher(server, networkEndpoint));
-    
+
     fakeView = EasyMock.createMock(AdminView.class);
-    
+    fakeServices = EasyMock.createMock(PlatformServices.class);
+
     // TODO: This probably needs to come off the UI thread as well.
     tagStore = new TagStore(new InMemoryStore());
     collectionStore = new CollectionStore(new InMemoryStore());
-    
-    ctrl = new Controller(collectionStore, tagStore, network);
-    
+
+    ctrl = new Controller(collectionStore, tagStore, network, fakeServices);
+
     ctrl.setAdminView(fakeView);
   }
-  
+
   public FakeDataSource getFakeServer() {
     return server;
   }
-  
+
   public final ThreadEndpoint<Request> networkEndpoint = new ThreadEndpoint<Request>() {
     @Override
     public void execute(Request msg) {
